6ae5d4f4ddf071ca0d61e12b1081cf96b2baabd2
Nuttha <nuthadit@gosoft.co.th>
2022-11-25 11:49:16 +0700

    update img

--- src/views/product_detail1.vue
+++ src/views/product_detail1.vue
@@ -5,7 +5,10 @@
     <div class="flex">
         <div class="col-6">
             <div style="text-align: center">
-                <img src="../assets/image/product1/Klothes1-1.png" alt="t-shirt" style="width: 70%" />
+                <!-- <img :src="img" alt="t-shirt" style="width: 70%" /> -->
+                <!-- <img src="../assets/image/product1/Klothes1-1.png" alt="t-shirt" style="width: 70%" /> -->
+                <img v-if="selectedColor" :src="require('@/assets/image/product1/' + selectedColor.src)" :alt="selectedColor.alt" style="width: 70%"/>
+                <img v-else :src="require('@/assets/image/product1/Klothes1-1.png')" alt="t-shirt-white" style="width: 70%"/>
             </div>
             <div
                 style="text-align: center; padding: 0px; margin-top: 20px"
@@ -18,7 +21,9 @@
         <div class="col-6">
             <div style="text-align: left">
                 <h2>{{t('color')}}</h2>
-                <Dropdown v-model="selectedColor" :options="colors" optionLabel="name" optionValue="code" :placeholder="t('selectAColor')" />
+                <Dropdown v-model="selectedColor" :options="colors" optionLabel="name"  :placeholder="t('selectAColor')" />
+                <!-- optionValue="name" -->
+                <p>{{selectedColor}}</p>
             </div>
             <div style="text-align: left">
                 <h2>{{t('size')}}</h2>
@@ -26,6 +31,12 @@
             </div>
             <div style="text-align: left">
                 <h2>{{t('price')}}</h2>
+            </div>
+            <div style="text-align: left">
+                <Button class="p-button-google p-3" aria-label="Purchase">
+                <i class="pi pi-shopping-cart px-2"></i>
+                <span class="px-3">Purchase</span>
+            </Button>
             </div>
         </div>
     </div>
@@ -37,24 +48,114 @@ import { ref } from 'vue';
 import { useI18n } from "vue-i18n";
 import Dropdown from 'primevue/dropdown';
 import SelectButton from 'primevue/selectbutton';
+import Button from 'primevue/button';
 const { t } = useI18n();
 
 const selectedColor = ref();
 const colors = ref([
-            {name: t('white'), code: 'K1-White'},
-            {name: t('pink'), code: 'K1-Pink'},
-            {name: t('purple'), code: 'K1-Purple'},
-            {name: t('cream'), code: 'K1-Cream'},
-            {name: t('black'), code: 'K1-Black'},
-            {name: t('green'), code: 'K1-Green'},
-            {name: t('navy'), code: 'K1-Navy'}
+            {name: t('white'), 
+            code: 'K1-White',
+            src: "Klothes1-1.png",
+            alt: "t-shirt-white"
+        },
+            {name: t('pink'), 
+            code: 'K1-Pink',
+            src: "Klothes1-2.png",
+            alt: "t-shirt-pink"
+        },
+            {name: t('purple'), 
+            code: 'K1-Purple',
+            src: "Klothes1-3.png",
+            alt: "t-shirt-purple"
+        },
+            {name: t('cream'), 
+            code: 'K1-Cream',
+            src: "Klothes1-4.png",
+            alt: "t-shirt-cream"
+        },
+            {name: t('black'), 
+            code: 'K1-Black',
+            src: "Klothes1-5.png",
+            alt: "t-shirt-black"
+        },
+            {name: t('green'), 
+            code: 'K1-Green',
+            src: "Klothes1-6.png",
+            alt: "t-shirt-green"
+        },
+            {name: t('navy'), 
+            code: 'K1-Navy',
+            src: "Klothes1-7.png",
+            alt: "t-shirt-navy"
+        }
         ]);
 
 const selectedSize = ref('XS');
 const sizes = ref(['XS', 'S' ,'M' ,'L' ,'XL']);
 
+const index = ref(0);
+const img = ref();
+const images = ref([{
+        id: 1,
+        src: "Klothes1-1.png",
+        alt: "t-shirt-white"
+    },
+    {
+        id: 2,
+        src: "../assets/image/product1/Klothes1-2.png",
+        alt: "t-shirt-pink"
+    },
+    {
+        id: 3,
+        src: "../assets/image/product1/Klothes1-3.png",
+        alt: "t-shirt-purple"
+    },
+    {
+        id: 4,
+        src: "../assets/image/product1/Klothes1-4.png",
+        alt: "t-shirt-cream"
+    },
+    {
+        id: 5,
+        src: "../assets/image/product1/Klothes1-5.png",
+        alt: "t-shirt-black"
+    },
+    {
+        id: 6,
+        src: "../assets/image/product1/Klothes1-6.png",
+        alt: "t-shirt-green"
+    },
+    {
+        id: 7,
+        src: "../assets/image/product1/Klothes1-7.png",
+        alt: "t-shirt-navy"
+    }
+    ])
+
+const switchImage = () =>{
+    img.value = images.value[index.value];
+    index.value = (index.value + 1) % images.value.length;
+}
 </script>
 
 
 <style>
+.p-button-google {
+    background: linear-gradient(to left, var(--purple-600) 50%, var(--purple-700) 50%);
+    background-size: 200% 100%;
+    background-position: right bottom;
+    transition: background-position 0.5s ease-out;
+    color: #fff;
+    border-color: var(--purple-700);
+}
+.p-button.google:hover {
+    background-position: left bottom;
+    background: linear-gradient(to right, var(--purple-600) 50%, var(--purple-700) 50%);
+}
+.p-button.google i {
+    background-color: var(--purple-700);
+}
+.p-button.google:focus {
+    box-shadow: 0 0 0 1px var(--purple-400);
+}
 </style>
